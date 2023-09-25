fa13abef286f6737c27c0c0c63711c093426e2a6
Villy Yeoh <yeohvilly@gmail.com>
2023-09-25 01:11:50 -0400

    added weather icon to the page

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var seachHistoryEl = document.getElementById("search-history");
 var searchResultEl = document.getElementById("search-result");
 var fiveDayForecastEl = document.getElementById("five-day-forecast");
 var cityEl = document.getElementById("city");
-var dateEl = document.getElementById("date");
+var iconEl = document.getElementById("icon");
 var tempEl = document.getElementById("temp");
 var windEl = document.getElementById("wind");
 var humidityEl = document.getElementById("humidity");
@@ -27,15 +27,26 @@ for (x = 0, y = 1; x < 5; x++, y++) {
 
 var weatherAPI = "https://api.openweathermap.org";
 var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+var apiUrl2 = "https://api.openweathermap.org/data/2.5/forecast?q="
 var apiKey = "0c4a0f7b9dff27d58bfb79aaa0d50f4c";
+var weatherIconUrl1 = "https://openweathermap.org/img/wn/";
+var weatherIconUrl2 = "@2x.png";
 // "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+// https://openweathermap.org/weather-conditions  (icons)
 // units=metric
+
+// 1) check if the data.list[0] is the same as today's date, if not,
+//    run 0, 8, 16, 24, 32, if yes, run 8, 16, 24, 32, 40.
+// 2) Display the weather icons with src, don't need to fetch again.
+// 3) Set up local storage, then search history bar.
+// 4) init function will call up local storage (optional) and then display 
+//    the latest successful search result.
 
 function init() {
     
     console.log(dayjs().format("MMM D, YYYY"));
     // this will be replaced by calling localstorage and display last saved
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
@@ -47,11 +58,16 @@ function init() {
         }
     })
 
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=ohio&appid=0c4a0f7b9dff27d58bfb79aaa0d50f4c&units=metric")
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);        
+                console.log(data);
+                
+                for (x = 0; x < data.list.length; x+=8) {
+                    console.log(data.list[x]);
+                }
+                console.log(parseInt(dayjs().format("D")), parseInt(data.list[0].dt_txt.split(" ")[0].split("-")[2]));
             })
         }
         else {
@@ -64,31 +80,49 @@ function findAPI() {
     var userInput = userInputEl.value;
     userInputEl.value = "";
     console.log(userInput);
+    
     fetch(apiUrl + userInput + "&appid=" + apiKey + "&units=metric")
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-                displayResult(data);
+                displayCurrentWeather(data);
             })
         }
         else {
             alert("Error: " + response.status);
         }
     })
+
+    fetch(apiUrl2 + userInput + "&appid=" + apiKey + "&units=metric")
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayFutureWeather(data);
+            })
+        }
+    })
 }
 
-function displayResult(data) {
-    cityEl.textContent = data.name + " ";
-    // this line is not showing up
-    dateEl.textContent = dayjs().format("MMM D, YYYY");
+function displayCurrentWeather(data) {
+    cityEl.textContent = data.name + " " + dayjs().format("MMM D, YYYY");
+    var weatherIcon = document.createElement("img");
+    weatherIcon.src = weatherIconUrl1 + data.weather[0].icon + weatherIconUrl2;
+
+    iconEl.src = weatherIconUrl1 + data.weather[0].icon + weatherIconUrl2;
+
+    console.log(weatherIcon.src);
     tempEl.textContent = "Temp: " + Math.round(data.main.temp) + "°C";
-    windEl.textContent = "Wind: " + data.wind.speed + "km/h";
+    windEl.textContent = "Wind: " + data.wind.speed + " km/h";
     humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
-
-    console.log();
+    console.log(dayjs().format("DD"));
 }
 
+function displayFutureWeather(data) {
+    console.log("displayFutureWeather");
+    
+}
 
 
 
